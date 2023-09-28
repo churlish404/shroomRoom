d5791cba8db5ffa3e34dc0f91d26f4e3d250a91c
lucaGilardenghi <luca.gilardenghi@yahoo.co.uk>
2023-09-28 18:58:47 +0100

    Spring setup

---
+++ server/shroom-api/src/main/java/com/example/shroomapi/ShroomApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shroomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShroomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShroomApiApplication.class, args);
+	}
+
+}
---
+++ server/shroom-api/src/test/java/com/example/shroomapi/ShroomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shroomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShroomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
